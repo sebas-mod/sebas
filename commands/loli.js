171b6b19793f800b92ec5aebffd0d1cd313b94a9
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-05-09 12:49:32 -0300

    Add files via upload

---
+++ commands/loli.js
@@ -0,0 +1,24 @@
+const mime = require("mime-types");
+
+module.exports = {
+    name: "loli",
+    category: "entertainment",
+    permissions: {
+        coin: 10
+    },
+    code: async (ctx) => {
+        try {
+            const result = tools.api.createUrl("archive", "/api/random/loli");
+
+            return await ctx.reply({
+                image: {
+                    url: result
+                },
+                mimetype: mime.lookup("png")
+            });
+        } catch (error) {
+            return await tools.cmd.handleError(ctx, error, true);
+        }
+    }
+};
+///// 
---
+++ commands/milf.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+const mime = require("mime-types");
+
+module.exports = {
+    name: "milf",
+    category: "entertainment",
+    permissions: {
+        premium: true
+    },
+    code: async (ctx) => {
+        try {
+            const result = tools.api.createUrl("nekorinn", "/waifuim/milf");
+
+            return await ctx.reply({
+                image: {
+                    url: result
+                },
+                mimetype: mime.lookup("png")
+            });
+        } catch (error) {
+            return await tools.cmd.handleError(ctx, error, true);
+        }
+    }
+};
+///////////
