34c0611915fa4097a512f9719e3d2b5a7fe8e423
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-05-09 14:48:03 -0300

    Update hidetag.js

--- commands/group/hidetag.js
+++ commands/group/hidetag.js
@@ -1,1 +1,28 @@
-const input = ctx.args.join(" ") || ctx.quoted.conversation || Object.values(ctx.quoted).map(v => v?.text || v?.caption).find(Boolean) || null;;
+const {
+    quote
+} = require("@mengkodingan/ckptw");
+
+module.exports = {
+    name: "hidetag",
+    aliases: ["ht"],
+    category: "group",
+    permissions: {
+        admin: true,
+        group: true
+    },
+    code: async (ctx) => {
+        const input = ctx.args.join(" ") || ctx.quoted.conversation || Object.values(ctx.quoted).map(v => v?.text || v?.caption).find(Boolean) || null;;
+
+        try {
+            const members = await ctx.group().members();
+            const mentions = members.map(m => m.id);
+
+            return await ctx.reply({
+                text: input,
+                mentions
+            });
+        } catch (error) {
+            return await tools.cmd.handleError(ctx, error, false);
+        }
+    }
+};
