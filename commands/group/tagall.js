96d5ea9b8ba3ce300c7f3dc589c149abb494d537
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-05-14 09:38:15 -0300

    Actualizar tagall.js

--- commands/group/tagall.js
+++ commands/group/tagall.js
@@ -3,33 +3,34 @@ const {
 } = require("@mengkodingan/ckptw");
 
 module.exports = {
-  name: "tagall",
-  category: "group",
-  desc: "Menciona a todos los participantes del grupo con imagen",
-  async run(client, message) {
-    // Verifica si es un grupo
-    if (!message.from.endsWith("@g.us")) {
-      return client.sendMessage(message.from, { text: '❌ Este comando solo puede usarse en grupos.' }, { quoted: message });
+    name: "todos",
+    category: "group",
+    permissions: {
+        admin: true,
+        group: true
+    },
+    code: async (ctx) => {
+        const input = ctx.args.join(" ") || ctx.quoted.conversation || Object.values(ctx.quoted).map(v => v?.text || v?.caption).find(Boolean) || null;;
+
+        try {
+            const members = await ctx.group().members();
+            const mentions = members.map(m => {
+                const serialized = tools.general.getID(m.id);
+                return {
+                    tag: `@${serialized}`,
+                    mention: m.id
+                };
+            });
+
+            const resultText = mentions.map(m => m.tag).join(" ");
+            return await ctx.reply({
+                text: `${input}\n` +
+                    `${config.msg.readmore}─────\n` +
+                    resultText,
+                mentions: mentions.map(m => m.mention)
+            });
+        } catch (error) {
+            return await tools.cmd.handleError(ctx, error, false);
+        }
     }
-
-    // Obtener miembros del grupo
-    const groupMetadata = await client.groupMetadata(message.from);
-    const participants = groupMetadata.participants;
-
-    const mentions = participants.map(p => p.id);
-    const tags = participants.map(p => `🔥 @${p.id.split("@")[0]}`).join('\n');
-
-    // Texto del mensaje
-    const caption = `👥 Total: ${participants.length} participantes.\nActivarse gente!\n─────\n${tags}\n\nby sebas - MD`;
-
-    // URL de imagen (puedes cambiarla por otra si quieres)
-    const imageUrl = 'https://uploader.nyxs.pw/tmp/CJ2b75-1747226092714.jpg';
-
-    // Enviar imagen con pie de foto y menciones
-    await client.sendMessage(message.from, {
-      image: { url: imageUrl },
-      caption: caption,
-      mentions: mentions
-    }, { quoted: message });
-  }
 };
