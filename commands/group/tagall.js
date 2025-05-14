4b9694a743cb6c06cb1fdbb24f375b83727b9920
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-05-14 11:04:16 -0300

    Actualizar tagall.js

--- commands/group/tagall.js
+++ commands/group/tagall.js
@@ -1,36 +1,42 @@
 const {
-    quote
+  quote
 } = require("@mengkodingan/ckptw");
 
 module.exports = {
-    name: "todos",
-    category: "group",
-    permissions: {
-        admin: true,
-        group: true
-    },
-    code: async (ctx) => {
-        const input = ctx.args.join(" ") || ctx.quoted.conversation || Object.values(ctx.quoted).map(v => v?.text || v?.caption).find(Boolean) || null;;
+  name: "tagall",
+  category: "group",
+  permissions: {
+    admin: true,
+    group: true
+  },
+  desc: "Menciona a todos los participantes del grupo con imagen",
+  code: async (ctx) => {
+    try {
+      const input = ctx.args.join(" ") || ctx.quoted?.conversation || Object.values(ctx.quoted || {}).map(v => v?.text || v?.caption).find(Boolean) || "¡Activarse gente!";
+      const group = await ctx.group();
+      const members = group.members();
 
-        try {
-            const members = await ctx.group().members();
-            const mentions = members.map(m => {
-                const serialized = tools.general.getID(m.id);
-                return {
-                    tag: `@${serialized}`,
-                    mention: m.id
-                };
-            });
+      const mentions = members.map(m => {
+        const serialized = tools.general.getID(m.id);
+        return {
+          tag: `🔥 @${serialized}`,
+          mention: m.id
+        };
+      });
 
-            const resultText = mentions.map(m => m.tag).join(" ");
-            return await ctx.reply({
-                text: `${input}\n` +
-                    `${config.msg.readmore}─────\n` +
-                    resultText,
-                mentions: mentions.map(m => m.mention)
-            });
-        } catch (error) {
-            return await tools.cmd.handleError(ctx, error, false);
-        }
+      const tags = mentions.map(m => m.tag).join("\n");
+      const caption = `👥 Total: ${members.length} participantes.\n${input}\n─────\n${tags}\n\nby sebas - MD`;
+
+      const imageUrl = 'https://t.me/+O4ZpMTe6KAJkYTBl';
+
+      return await ctx.reply({
+        image: { url: imageUrl },
+        caption,
+        mentions: mentions.map(m => m.mention)
+      });
+
+    } catch (err) {
+      return await tools.cmd.handleError(ctx, err, false);
     }
+  }
 };
