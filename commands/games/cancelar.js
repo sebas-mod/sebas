b60aac1c49bcd087fe3cda2e97fecbd560aef603
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-05-14 12:57:07 -0300

    Add files via upload

---
+++ commands/games/cancelar.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "cancelar",
+  category: "games",
+  permissions: {
+    group: true
+  },
+  code: async (ctx) => {
+    const groupId = ctx.chat;
+    global.juegos = global.juegos || {};
+
+    if (!global.juegos?.[groupId]) {
+      return await ctx.reply("❌ No hay un juego de ahorcado activo en este grupo.");
+    }
+
+    delete global.juegos[groupId];
+    return await ctx.reply("🛑 El juego de ahorcado ha sido cancelado.");
+  }
+};
